1ebc15a12944bed74af78596c4e0c819bcb99c08
TageZ <tagezerby@gmail.com>
2024-05-06 19:40:51 -0500

    Fixing Web Socket Rooms

--- frontend/src/pages/Game.js
+++ frontend/src/pages/Game.js
@@ -22,46 +22,7 @@ function Game({user}) {
     const [response, setResponse] = useState('Connected to server')
     const [users, setUsers] = useState([])
     const { code } = useParams(); // This retrieves the game code from the URL
-    const game_room = code
 
-    // useEffect(() => {
-    //     // Connect to the server
-    //     const socketUrl = `http://localhost:3306/`;
-    //     const newSocket = io(socketUrl, { path: '/socket.io' });
-    //     setSocket(newSocket);
-    
-    //     // Listen for connection event
-    //     newSocket.on('connect', () => {
-    //         console.log('Connected to server');
-    //         setResponse('Connected to server');
-    //     });
-    
-    //     // Listen for disconnect event
-    //     newSocket.on('disconnect', () => {
-    //         console.log('Disconnected from server');
-    //         setResponse('Disconnected from server');
-    //     });
-
-    //     newSocket.on('moveResponse', (response) => {
-    //         console.log('Received move response:', response);
-    //         setOtherBoard(true);
-    //         setResponse('Recieved response: ' + response)
-    //     });
-
-    //     newSocket.on('getUsers', (users) => {
-    //         setUsers(users);
-    //         console.log(users)
-    //     });
-
-    //     newSocket.emit('join', `${game_room}, ${user}`);
-    //     console.log(`WebSocket: User ${user} joined room: ${game_room}`);
-    //     newSocket.emit('getUsers', `${game_room}`);
-    
-    //     // Clean up on unmount
-    //     return () => {
-    //         newSocket.disconnect();
-    //     };
-    // }, []);
 
     useEffect(() => {
         // Connect to the server
@@ -72,6 +33,7 @@ function Game({user}) {
         // Listen for connection event
         newSocket.on('connect', () => {
             console.log('Connected to server');
+            newSocket.emit('join', `${code}, username`);
             setResponse('Connected to server');
         });
     
@@ -100,7 +62,7 @@ function Game({user}) {
 
     useEffect(() => {
         if (socket) {
-            socket.emit('updateBoard', "UPDATE BOARD");
+            socket.emit('updateBoard', code);
         }
     }, [newBoard]);
 
